24a869b899a341499d4c0393a46a1b9ef811e2ab
Chay23 <Chay23@users.noreply.github.com>
2020-05-22 23:19:22 +0300

    add cap files

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@
 
 # set :deploy_to, "/home/#{user}/deploy/#{fetch(:application)}"
 
-user = 'chaii'
+user = 'Chay23'
 domain = 'shop.yourhost.com'
 # adjust if you are using RVM, remove if you are not
 set :rvm_type, :system
@@ -28,6 +28,3 @@ set :repo_url, "#{user}@#{domain}:git/#{fetch(:application)}.git"
 set :deploy_to, "/home/chaii/apps_rails/#{fetch(:application)}"
 # distribute your applications across servers (the instructions below put them
 # all on the same server, defined above as 'domain', adjust as necessary)
-role :app, domain
-role :web, domain
-role :db, domain

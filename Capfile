1664ac3620dbce79a59f53d4db70960d9b82c4bd
Chay23 <Chay23@users.noreply.github.com>
2020-05-24 20:26:54 +0300

    cap production

--- Capfile
+++ Capfile
@@ -34,5 +34,8 @@ install_plugin Capistrano::SCM::Git
  require "capistrano/rails/migrations"
  require "capistrano/passenger"
 
+ set :rbenv_type, :user
+ set :rbenv_ruby, '2.7.1'
+
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,14 @@
-user = 'chaii'
-domain = 'shop.yourhost.com'
-# adjust if you are using RVM, remove if you are not
-set :rvm_type, :system
-set :rvm_ruby_string, 'ruby-2.7.1'
-# name of your application
-set :application, 'ShopRails'
-# file paths
-set :repo_url, "#{user}@#{domain}:git/#{fetch(:application)}.git"
-set :deploy_to, "/home/#{user}/deploy/#{fetch(:application)}"
-# distribute your applications across servers (the instructions below put them
-# all on the same server, defined above as 'domain', adjust as necessary)
-role :app, domain
-role :web, domain
-role :db, domain
-# As Capistrano executes in a non-interactive mode and therefore doesn't cause
-# any of your shell profile scripts to be run, the following might be needed
-# if (for example) you have locally installed gems or applications. Note:
-# this needs to contain the full values for the variables set, not simply
-# the deltas.
-#
-# set :default_environment, {
-# 'PATH' => '<your paths>:/usr/local/bin:/usr/bin:/bin',
-# 'GEM_PATH' => '<your paths>:/usr/lib/ruby/gems/1.8'
-# }
-#
-# See https://rvm.io/deployment/capistrano#environment for more info.
+set :application, "ShopRails"
+set :repo_url, "git@github.com:Chay23/ShopRails.git"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/chaii/apps_rails/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
+# Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
+# This is useful if you don't want to use ENV variables
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
